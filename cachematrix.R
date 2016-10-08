f966fc6551e0947de4deb76ea15401e06ec9f7d6
yaramansour <yaramansour@outlook.com>
2016-10-08 14:46:47 +0200

    Add files via upload
    MY FINAL SUBMISSION

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## function that set and get the value of the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -13,7 +11,9 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   ##getting the matrix data 
   setInverse <- function(inverse) inv <<- inverse
+  ## setting the inverse of the matrix
   getInverse <- function() inv
+  ## get the inverse of the matrix that's already calculated 
   list(set = set,
        get = get,
        setInverse = setInverse,
@@ -22,17 +22,25 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## function that calculate the inverse of a matrix if not calculated before
+## and just gets if if it's calculated before 
 
 cacheSolve <- function(x, ...) {
+  #takes the value of inv from getinverse in the previous function 
   inv <- x$getInverse()
+  #compare the value of ive 
+  #if NOT equal to nul it means tha the inverse is calculated before for this matrix
   if (!is.null(inv)) {
     message("cached data")
     return(inv)
   }
+  # else it gets the data of the matrix with function get()
   mat <- x$get()
+  # then use solve() to get the inverse of the matrix
   inv <- solve(mat,...)
+  # set the inv to the value calculated by solve
   x$setInverse(inv)
-  message("calculating ...")
+  #return the inverse
+  message("calculating ..")
   inv
 }
